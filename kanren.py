2ee5dc87c0546b2842e45f2bd7baa90cd43fe08b
James Tauber <jtauber@jtauber.com>
2012-08-03 03:58:40 -0400

    non-idiomatic lookup

--- kanren.py
+++ kanren.py
@@ -15,3 +15,18 @@ class Var:
 empty_s = {}
 s1 = {Var("x"): 5, Var("y"): True}
 s2 = {Var("y"): 5, Var("x"): Var("y")}
+
+
+def lookup(v, s):
+    if isinstance(v, Var):
+        a = s.get(v)
+        if a:
+            return a
+        else:
+            return v
+    else:
+        return v
+
+
+assert lookup(Var("x"), s1) == 5
+assert lookup(Var("x"), s2) == Var("y")
