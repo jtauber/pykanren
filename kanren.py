f292442fef852e85f883800f1b028f02f9370c08
James Tauber <jtauber@jtauber.com>
2012-08-04 02:15:19 -0400

    split tests out of kanren module

--- kanren.py
+++ kanren.py
@@ -12,11 +12,6 @@ class Var:
         return "<%s>" % self.symbol
 
 
-empty_s = {}
-s1 = {Var("x"): 5, Var("y"): True}
-s2 = {Var("y"): 5, Var("x"): Var("y")}
-
-
 # more idiomatic would be:
 #     return s.get(v, v) if isinstance(v, Var) else v
 # but this parallels walk below better
@@ -31,10 +26,6 @@ def lookup(v, s):
         return v
 
 
-assert lookup(Var("x"), s1) == 5
-assert lookup(Var("x"), s2) == Var("y")
-
-
 def walk(v, s):
     if isinstance(v, Var):
         a = s.get(v)
@@ -46,10 +37,6 @@ def walk(v, s):
         return v
 
 
-assert walk(Var("x"), s1) == 5
-assert walk(Var("x"), s2) == 5
-
-
 def occurs_check(x, v, s):
     v = walk(v, s)
     if isinstance(v, Var):
@@ -59,23 +46,12 @@ def occurs_check(x, v, s):
         return False
 
 
-s3 = {Var("x"): Var("y")}
-assert occurs_check(Var("y"), Var("x"), s3) == True
-assert occurs_check(Var("y"), 5, s3) == False
-assert occurs_check(Var("x"), Var("x"), {}) == True
-
-
 def extend_s(x, v, s):
     if occurs_check(x, v, s):
         return False
     else:
         s[x] = v
         return s
-
-
-assert extend_s(Var("y"), Var("x"), s3) == False
-assert extend_s(Var("y"), 5, s3) == {Var("y"): 5, Var("x"): Var("y")}
-assert extend_s(Var("x"), Var("x"), {}) == False
 
 
 def unify(u, v, s):
@@ -98,16 +74,6 @@ def unify(u, v, s):
         return False
 
 
-assert unify(None, 1, {}) == False
-assert unify(None, Var("x"), {}) == {Var("x"): None}
-assert unify(1, None, {}) == False
-assert unify(1, 1, {}) == {}
-assert unify(1, 2, {}) == False
-assert unify(1, Var("x"), {}) == {Var("x"): 1}
-assert unify(Var("x"), 1, {}) == {Var("x"): 1}
-assert unify(Var("x"), Var("y"), {}) == {Var("x"): Var("y")}
-
-
 def unify_no_check(u, v, s):
     u = walk(u, s)
     v = walk(v, s)
@@ -124,13 +90,3 @@ def unify_no_check(u, v, s):
         return s
     else:
         return False
-
-
-assert unify_no_check(None, 1, {}) == False
-assert unify_no_check(None, Var("x"), {}) == {Var("x"): None}
-assert unify_no_check(1, None, {}) == False
-assert unify_no_check(1, 1, {}) == {}
-assert unify_no_check(1, 2, {}) == False
-assert unify_no_check(1, Var("x"), {}) == {Var("x"): 1}
-assert unify_no_check(Var("x"), 1, {}) == {Var("x"): 1}
-assert unify_no_check(Var("x"), Var("y"), {}) == {Var("x"): Var("y")}
---
+++ tests.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from kanren import *
+
+
+s1 = {Var("x"): 5, Var("y"): True}
+s2 = {Var("y"): 5, Var("x"): Var("y")}
+
+
+assert lookup(Var("x"), s1) == 5
+assert lookup(Var("x"), s2) == Var("y")
+
+
+assert walk(Var("x"), s1) == 5
+assert walk(Var("x"), s2) == 5
+
+
+s3 = {Var("x"): Var("y")}
+assert occurs_check(Var("y"), Var("x"), s3) == True
+assert occurs_check(Var("y"), 5, s3) == False
+assert occurs_check(Var("x"), Var("x"), {}) == True
+
+
+assert extend_s(Var("y"), Var("x"), s3) == False
+assert extend_s(Var("y"), 5, s3) == {Var("y"): 5, Var("x"): Var("y")}
+assert extend_s(Var("x"), Var("x"), {}) == False
+
+
+assert unify(None, 1, {}) == False
+assert unify(None, Var("x"), {}) == {Var("x"): None}
+assert unify(1, None, {}) == False
+assert unify(1, 1, {}) == {}
+assert unify(1, 2, {}) == False
+assert unify(1, Var("x"), {}) == {Var("x"): 1}
+assert unify(Var("x"), 1, {}) == {Var("x"): 1}
+assert unify(Var("x"), Var("y"), {}) == {Var("x"): Var("y")}
+
+
+assert unify_no_check(None, 1, {}) == False
+assert unify_no_check(None, Var("x"), {}) == {Var("x"): None}
+assert unify_no_check(1, None, {}) == False
+assert unify_no_check(1, 1, {}) == {}
+assert unify_no_check(1, 2, {}) == False
+assert unify_no_check(1, Var("x"), {}) == {Var("x"): 1}
+assert unify_no_check(Var("x"), 1, {}) == {Var("x"): 1}
+assert unify_no_check(Var("x"), Var("y"), {}) == {Var("x"): Var("y")}
