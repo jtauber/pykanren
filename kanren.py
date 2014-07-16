b40bb18bd0048f7c9a31741287ed7a2866d10268
James Tauber <jtauber@jtauber.com>
2014-07-15 23:40:19 -0400

    whitespace

--- kanren.py
+++ kanren.py
@@ -7,13 +7,13 @@
 class Var:
     def __init__(self, symbol):
         self.symbol = symbol
-    
+
     def __eq__(self, other):
         return isinstance(other, Var) and self.symbol == other.symbol
-    
+
     def __hash__(self):
         return hash(self.symbol)
-    
+
     def __repr__(self):
         return "<%s>" % self.symbol
 
